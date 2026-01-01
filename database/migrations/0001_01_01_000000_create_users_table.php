416e7d69a7c11aa4f872ae6333099782fc0dc1dd
Raihan Syeka Pramukastie <rehaansekap@gmail.com>
2026-01-01 12:43:02 +0700

    fix: email_verified_at migrations at users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username')->unique()->nullable();
+            $table->string('username')->unique()->nullable(); // NIS/NIP
             $table->string('email')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['student', 'teacher'])->default('student');
 
