a8a2637cc4b52f6fee1d4710ada32e2c1d3803f2
Raihan Syeka Pramukastie <rehaansekap@gmail.com>
2026-01-12 07:59:18 +0700

    refactor: simplify grades table by consolidating score fields into a single score column

--- database/migrations/2026_01_01_052812_create_grades_table.php
+++ database/migrations/2026_01_01_052812_create_grades_table.php
@@ -16,12 +16,8 @@ return new class extends Migration
             $table->foreignId('submission_id')->constrained()->onDelete('cascade');
             $table->foreignId('teacher_id')->constrained('users');
 
-            $table->integer('score_clarification')->default(0);
-            $table->integer('score_assessment')->default(0);
-            $table->integer('score_strategy')->default(0);
-            $table->integer('score_conclusion')->default(0);
+            $table->integer('score')->default(0);
 
-            $table->integer('score_collaboration')->default(0);
             $table->text('teacher_notes')->nullable();
 
             $table->timestamps();
